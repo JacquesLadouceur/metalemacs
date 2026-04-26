c6f1aad40b3bd495e11cea87f8ee4623708d1ef1
Jacques Ladouceur <jacques.ladouceur@lli.ulval.ca>
2026-04-26 18:10:21 -0400

    Exclure metal-prefs.el (préférences locales)

--- metal-prefs.el
+++
@@ -1,8 +0,0 @@
-;; Preferences MetalEmacs - genere automatiquement
-(setq metal-font-size-offset 20)
-(setq metal-treemacs-width 40)
-(setq metal-frame-left 78)
-(setq metal-frame-top 56)
-(setq metal-frame-px-width 1286)
-(setq metal-frame-px-height 701)
-(setq metal-frame-fullscreen nil)
